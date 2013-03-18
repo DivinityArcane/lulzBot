0d70cfd6cc4e3ff23c79e2ee593dbc11a7fc0aca
Justin Eittreim <eittreim.justin@live.com>
2013-03-17 19:19:15 -0700

    Change version back to 'Development' for source.

--- lulzbot/Program.cs
+++ lulzbot/Program.cs
@@ -119,7 +119,7 @@ namespace lulzbot
         public static List<String> OfficialChannels = new List<String>() { "#devart", "#help", "#mnadmin", "#seniors", "#communityrelations" };
         public static List<String> NoDisplay = new List<String>() { "#datashare", "#dsgateway" };
         public const String BotName = "lulzBot";
-        public const String Version = "1.03";
+        public const String Version = "1.03 Development";
         public const String ReleaseName = "Synergy";
 
         static void Main (string[] args)
